a8622b04d4dd1b34bcafeca434354490565606f5
Gabriel <giurushima@hotmail.com>
2025-03-09 02:42:29 -0300

    ADD: DTO para creacion y actualizacion de choferes.

---
+++ Application/Models/Truck/TruckDto.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Truck
+{
+    public class CreateTruckDto
+    {
+        [Required(ErrorMessage = "Ingrese nombre completo (menor a 32 caracteres)")]
+        [MaxLength(32)]
+        public string? CompleteName { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Ingrese tipo de carga (menor a 32 caracteres)")]
+        [MaxLength(32)]
+        public string? TruckerType { get; set; }
+        public Roles Roles { get; set; }
+    }
+
+    public class UpdateTruckDto
+    {
+        [Required(ErrorMessage = "Ingrese nombre completo (menor a 32 caracteres)")]
+        [MaxLength(32)]
+        public string? CompleteName { get; set; }
+        [Required(ErrorMessage = "Ingrese tipo de carga (menor a 32 caracteres)")]
+        [MaxLength(32)]
+        public string? TruckerType { get; set; }
+        public Roles Roles { get; set; }
+    }
+}
