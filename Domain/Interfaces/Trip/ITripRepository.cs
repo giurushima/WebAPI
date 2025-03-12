2f727f4dcc07a27c56d7bf91af5c2bdbc69aa356
Gabriel <giurushima@hotmail.com>
2025-03-12 10:51:43 -0300

    CHANGE: Cambios en nombre de metodos.

--- Domain/Interfaces/Trip/ITripRepository.cs
+++ Domain/Interfaces/Trip/ITripRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,11 @@ namespace Domain.Interfaces.Trips
 {
     public interface ITripRepository
     {
-        Task<IEnumerable<Trip>> GetTripsByTruckerAsync(int truckerId);
-        Task<Trip?> GetTripByTruckerAndTripIdAsync(int truckerId, int tripId);
-        Task AddTripAsync(Trip trip);
-        Task UpdateTripAsync(Trip trip);
-        Task DeleteTripAsync(int id);
+        Task<IEnumerable<Trip>> GetTripsByTrucker(int truckerId);
+        Task<Trip?> GetTripByTruckerAndTripId(int truckerId, int tripId);
+        Task AddTrip(Trip trip);
+        Task UpdateTrip(Trip trip);
+        Task DeleteTrip(int id);
+        Task<IEnumerable<Trip>> GetTripsByStatus(TripStatus status);
     }
 }
